a176b0430553e75d001d219b34c83ea5a5b57df0
Nijat Muhtar <nijatmn@gmail.com>
2022-07-24 15:19:36 -0400

    latest change for nadira

---
+++ src/test/java/operators/IncrementDecrementTest.java
@@ -0,0 +1,69 @@
+package operators;
+
+import org.testng.annotations.Test;
+
+public class IncrementDecrementTest {
+
+    @Test
+    public void increment() {
+        // symbol  ++
+        // adds 1
+        // variable content updater by 1
+        int num = 80;
+        num++;  // postfix
+        System.out.println(num);
+
+        int age = 10;
+        ++age;  // prefix
+        System.out.println(age);
+    }
+
+    @Test
+    public void decrement() {
+        // symbol --
+        // subtracts 1
+        // variable content update by -1
+        int number = 100;
+        number--;   // postfix
+        System.out.println(number);
+
+        int limit = 100;
+        --limit;    // prefix
+        System.out.println(limit);
+    }
+
+    @Test
+    public void postfixAndPrefix() {
+        // postfix:   var++,   var--
+        // prefix :   ++var,   --var
+
+        int n = 15;
+        System.out.println( n--); // 15
+        System.out.println( n );  // 14
+
+
+        int y = 20;
+        System.out.println(++y);
+    }
+
+    @Test
+    public void orderOfExecution() {
+        int a = 2;
+        int b = 4;
+        int c = 10;
+
+        System.out.println(a + b-- * c);
+        System.out.println(b);
+    }
+
+    @Test
+    public void ocaQuizExample() {
+        int n = 10;
+        int x = 20;
+
+        int ret = ++n * x-- + 100;
+
+        System.out.println(ret);
+    }
+
+}
