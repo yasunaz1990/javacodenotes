dfb2b70470ad956b5a1149d32846c4a7571fa874
Russell Ratanamalaya <russellratanamalaya@gmail.com>
2022-07-26 19:41:21 -0400

    added assignment

---
+++ src/test/java/assignment/Assignment1.java
@@ -0,0 +1,7 @@
+package assignment;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        System.out.println("it is I russell");
+    }
+}
--- src/test/java/operators/varContentUpdaterTest.java
+++ src/test/java/operators/varContentUpdaterTest.java
@@ -1,2 +1,69 @@
-package operators;public class varContentUpdaterTest {
+package operators;
+import org.testng.annotations.Test;
+
+public class varContentUpdaterTest {
+
+    @Test
+    public void addUpdate(){
+        int number = 100;
+        number += 20;
+        System.out.println(number);
+
+        int age = 30;
+        age = age + 10;
+        System.out.println(age);
+    }
+
+    @Test
+    public void minusUpdate(){
+        int number = 100;
+        number -= 20;
+        System.out.println(number);
+
+        int age = 30;
+        age = age - 10;
+        System.out.println(age);
+    }
+
+    @Test
+    public void multiplyUpdate(){
+        int number = 100;
+        number *= 20;
+        System.out.println(number);
+
+        int age = 30;
+        age = age * 10;
+        System.out.println(age);
+    }
+
+    @Test
+    public void divisionUpdate(){
+        int number = 100;
+        number /= 20;
+        System.out.println(number);
+
+        int age = 30;
+        age = age / 10;
+        System.out.println(age);
+    }
+
+    @Test
+    public void modUpdate(){
+        int number = 100;
+        number %= 20;
+        System.out.println(number);
+
+        int age = 30;
+        age = age % 10;
+        System.out.println(age);
+    }
+
+    @Test
+    public void incrementDecrementUpdater(){
+        char letter = 'A';
+        letter++;
+        System.out.println(letter);
+
+    }
+
 }
