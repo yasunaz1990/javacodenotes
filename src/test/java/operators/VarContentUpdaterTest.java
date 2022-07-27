07df101818b5b17184fa7483d7a065694bb34cb9
Alba Rodriguez <albatrainer@yahoo.es>
2022-07-26 17:04:30 -0700

    latest

---
+++ src/test/java/operators/VarContentUpdaterTest.java
@@ -0,0 +1,59 @@
+package operators;
+
+import org.testng.annotations.Test;
+
+public class VarContentUpdaterTest {
+    @Test
+    public void addUpdate() {
+        // symbol +=
+        int number = 100;
+        number += 20;
+        System.out.println(number);
+
+        // old way
+        int age = 20;
+        age = age + 10;
+        System.out.println(age);    // 30
+    }
+    @Test
+    public void subtractUpdate() {
+        // symbol -=
+        double number = 100;
+        number -= 32;
+        System.out.println(number);
+    }
+    @Test
+    public void divisionUpdate() {
+        // symbol /=
+        double number = 100;
+        number /= 2;
+        System.out.println(number);
+    }
+    @Test
+    public void multiUpdater() {
+        // symbol *=
+        int number = 20;
+        number *= 3;
+        System.out.println(number);
+    }
+    @Test
+    public void modulusUpdater() {
+        // symbol %=
+        int number = 10;
+        number %= 3
+        System.out.println(number);
+    }
+    @Test
+    public void incrementDecrementUpdater() {
+        // symbol ++, --
+        // only works on numeric vara ( double, int, char )
+        char letter = 'A';
+        letter++;
+        System.out.println(letter);
+
+        int num = 10;
+
+        int emp;
+        emp += 10;
+    }
+}
